833cc6443e8592711da733792f971b8ef9b744f6
Kukushouhou <1@kukushouhou.com>
2026-06-01 16:36:14 +0800

    chore: 添加 Jest 测试基础设施配置

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: ['src/interface.ts'],
+};
+
+module.exports = config;
